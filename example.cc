a85c17786a90464f87979a8aed1a5e8c0c9c06f3
Maxime Rochkoulets <maxime.rochkoulets@etu.u-bordeaux.fr>
2023-03-24 00:24:00 +0100

    Corrected minor bug + simplified the code.

--- example.cc
+++ example.cc
@@ -5,45 +5,43 @@
 using namespace std;
 
 int main() {
-  srand(time(nullptr));
-  int n_to_insert = 100'000;
+    srand(time(nullptr));
+    int n_to_insert = 100'000;
 
-  cout << "Creating a Min-Heap:\n\n";
-  Heap<int> *min_heap = new Heap<int>;
+    cout << "Creating a Min-Heap:\n\n";
+    Heap<int> *min_heap = new Heap<int>;
 
-  cout << "Inserting 100000 random integers in the Min-Heap...\n\n";
-  cout.flush();
-  for (int i = n_to_insert; i > 0; --i)
-    min_heap->insert(rand() % 1000001);
+    cout << "Inserting 100000 random integers in the Min-Heap...\n\n";
+    cout.flush();
+    for (int i = n_to_insert; i > 0; --i) min_heap->insert(rand() % 1000001);
 
-  // Removing and printing the top element.
-  for (int i = 0; i < 3; ++i) {
-    cout << "Removing the top element.\n";
-    cout << "Top Element => " << min_heap->pop_top() << '\n';
-    cout << "Heap Size => " << min_heap->get_size() << "\n";
-    cout << "_____\n";
-  }
+    // Removing and printing the top element.
+    for (int i = 0; i < 3; ++i) {
+        cout << "Removing the top element.\n";
+        cout << "Top Element => " << min_heap->pop_top() << '\n';
+        cout << "Heap Size => " << min_heap->get_size() << "\n";
+        cout << "_____\n";
+    }
 
-  delete (min_heap);
+    delete (min_heap);
 
-  cout << "\nCreating a Max-Heap:\n\n";
-  auto compare = [](int a, int b) { return a < b; };
-  Heap<int> *max_heap = new Heap<int>(compare);
+    cout << "\nCreating a Max-Heap:\n\n";
+    auto compare        = [](int a, int b) { return a < b; };
+    Heap<int> *max_heap = new Heap<int>(compare);
 
-  cout << "Inserting 100000 random integers in the Max-Heap...\n\n";
-  cout.flush();
-  for (; n_to_insert > 0; --n_to_insert)
-    max_heap->insert(rand() % 1000001);
+    cout << "Inserting 100000 random integers in the Max-Heap...\n\n";
+    cout.flush();
+    for (; n_to_insert > 0; --n_to_insert) max_heap->insert(rand() % 1000001);
 
-  // Removing and printing the top element.
-  for (int i = 0; i < 3; ++i) {
-    cout << "Removing the top element.\n";
-    cout << "Top Element => " << max_heap->pop_top() << '\n';
-    cout << "Heap Size => " << max_heap->get_size() << "\n";
-    cout << "_____\n";
-  }
+    // Removing and printing the top element.
+    for (int i = 0; i < 3; ++i) {
+        cout << "Removing the top element.\n";
+        cout << "Top Element => " << max_heap->pop_top() << '\n';
+        cout << "Heap Size => " << max_heap->get_size() << "\n";
+        cout << "_____\n";
+    }
 
-  delete (max_heap);
+    delete (max_heap);
 
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
